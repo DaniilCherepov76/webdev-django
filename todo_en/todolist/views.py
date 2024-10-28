420d4c7c78925d241541ed3a88d8eec5c73b84d2
DaniilCherepov76 <pz38na79@gmail.com>
2024-10-28 21:57:32 +0300

    Add files via upload

---
+++ todo_en/todolist/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .models import TodoItem
+
+# Create your views here.
+def todoView(request):
+    all_todo_items = TodoItem.objects.all()
+    return render(request, 'todolist.html', {'all_items': all_todo_items})
+
+def addTodoView(request):
+    x = request.POST['content']
+    new_item = TodoItem(content = x)
+    new_item.save()
+    return HttpResponseRedirect('/todolist/')
